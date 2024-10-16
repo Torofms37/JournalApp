3f3f92d9e61364e8f545f833f022e87b78a9bf4e
torofms37 <torofms37@gmail.com>
2024-10-16 08:54:29 -0700

    notginh

--- src/views/NothingSelectedView.jsx
+++ src/views/NothingSelectedView.jsx
@@ -1,5 +1,15 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 export const NothingSelectedView = () => {
-  return <div>NothingSelectedView</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    ></Grid>
+  );
 };
