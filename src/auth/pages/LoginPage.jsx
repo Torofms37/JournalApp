bbff7f846b659dddb4b3128f8c4344135ba8a472
torofms37 <torofms37@gmail.com>
2024-11-26 07:06:53 -0700

    termino de la sseccion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -39,7 +39,10 @@ export const LoginPage = () => {
   };
   return (
     <AuthLayout title="Logeate">
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        className="animate__animated animate__fadeIn animate__faster"
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -63,7 +63,10 @@ export const RegisterPage = () => {
   return (
     <AuthLayout title="Registro">
       <h1>FormValid: {isFormValid ? "Valido" : "Invalido"}</h1>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        className="animate__animated animate__fadeIn animate__faster"
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
